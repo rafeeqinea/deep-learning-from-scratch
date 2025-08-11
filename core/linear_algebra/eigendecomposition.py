eff7f00843909adc96f04443e411d9e4fe1207be
weeoto <99127292+rafeeqinea@users.noreply.github.com>
2025-08-11 09:54:39 +0100

    gg

--- core/linear_algebra/eigendecomposition.py
+++
@@ -1,8 +0,0 @@
-# ==============================================================================
-# 📄 Filename        : eigendecomposition.py
-# ✍️  Author         : Mohammed Rafeeq Faraaz Shaik (@rafeeqinea)
-# 📅  Created on     : 2025-08-05
-# 📘  Deep Learning Book Chapter : Chapter <CHAPTER> — <TITLE>
-# 📖  Page Number    : Page <PAGE>
-# 🔧  Description    : <DESCRIPTION>
-# ==============================================================================
