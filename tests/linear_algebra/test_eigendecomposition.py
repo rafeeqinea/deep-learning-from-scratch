39aada077dc24a28ad10c0a4b2cd81e7ceeb1bc5
weeoto <99127292+rafeeqinea@users.noreply.github.com>
2025-10-08 17:09:50 +0100

    Remove core and tests folders from repository

--- tests/linear_algebra/test_eigendecomposition.py
+++
@@ -1,3 +0,0 @@
-import pytest
-
-pytestmark = pytest.mark.skip("eigendecomposition not yet implemented")
--- tests/linear_algebra/test_norms.py
+++
@@ -1,3 +0,0 @@
-import pytest
-
-pytestmark = pytest.mark.skip("matrix/vector norms module not yet implemented (separate from vectors.vector_norm)")
